4f7dbffc8d127d5c72e028d065883107a4f5fc68
josimar10b <josimar10brb@gmail.com>
2023-06-27 11:35:52 -0500

    commit paginado funcionando

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,21 +1,52 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { CardsContainer } from "../../components/Cards/CardsContainer";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllProducts } from "../../redux/actions";
-import styles from "./Home.module.css";
+import { Pagination } from "@mui/material";
+import { Box, margin } from "@mui/system";
 
 const Home = () => {
   const dispatch = useDispatch();
   const { allProducts } = useSelector((state) => state);
 
+  const [page, setPage] = useState(1)
+  const [cardsPerPage, setCardsPerPage] = useState(6)
+
+  
+  
   useEffect(() => {
     dispatch(getAllProducts());
   }, [dispatch]);
+  
+  const handleChange = (event, value) => {
+    setPage(value)
+  }
+  
+  const indexOfLastCard = page * cardsPerPage;
+  const indexOfFirstCard = indexOfLastCard - cardsPerPage;
+  const currentCards = allProducts.slice(indexOfFirstCard, indexOfLastCard);
 
   console.log(allProducts);
   return (
     <div>
-      <CardsContainer allProducts={allProducts} />
+      <Box sx = {{width:"100%", display:"flex", justifyContent: "center", marginTop: "20px", marginBottom: "20px"}}>
+        <Pagination 
+            variant="outlined" 
+            color="secondary" 
+            count={Math.ceil(allProducts.length / cardsPerPage)} 
+            page={page} 
+            onChange={handleChange}
+            size="large"
+            sx={{
+              "& .MuiPaginationItem-root": {
+                color: "yellow", // Cambiar el color de los números
+              },
+              "& .MuiPaginationItem-icon": {
+                color: "#f44336", // Cambiar el color de las flechas de "Next" y "Prev"
+              }}}
+         />
+      </Box>
+      <CardsContainer allProducts={currentCards} />
     </div>
   );
 };
