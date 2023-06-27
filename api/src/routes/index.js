87baffb69e18d0f37570c25293a7fc1fb30f0d02
BenBerg02 <preguntasboludas123@gmail.com>
2023-06-27 19:47:35 -0300

    Revert "added the model, router and controllers, it's needed to test the remover of products of cart"
    This reverts commit 7cb3594c733a42bdfd54a89d70c1f9b138a612f5.

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,12 +4,10 @@ const admin = require("./Admin");
 const productRouter = require('./product.js');
 const router = Router();
 const categoria = require("./category")
-const Cart = require("./Cart")
 
 router.use('/products', productRouter);
 router.use("/users", user);
 router.use("/admin", admin);
 router.use("/categoria", categoria);
-router.use("/cart", Cart)
 
 module.exports = router;
