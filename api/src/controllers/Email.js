700de4bd81af22a6575bee4a79ee1f9d5bc315e9
Rafael <roboticra@gmail.com>
2023-06-30 21:39:43 -0500

    primer metodo para el envio de correos

---
+++ api/src/controllers/Email.js
@@ -0,0 +1,32 @@
+const nodemailer = require("nodemailer");
+
+const { EMAILACCOUNT, PASSWORDEMAIL, SMTPHOST, SMTPPORT } = process.env;
+
+async function sendEmail(from, to) {
+  const client = nodemailer.createTransport({
+    host: SMTPHOST,
+    port: SMTPHOST,
+    auth: {
+      user: EMAILACCOUNT,
+      pass: PASSWORDEMAIL,
+    },
+    requireTLS: true,
+  });
+
+  return await client.sendMail(
+    {
+      to,
+      from,
+      html: "<div>hola mundo</div>",
+      subject: "HENRY ESCLAVISTA XD",
+    },
+    (error) => {
+      if (error) {
+        throw Error(error);
+      }
+      return true;
+    }
+  );
+}
+
+module.exports = sendEmail;
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,19 +1,20 @@
 const { Router } = require("express");
 const user = require("./user");
 const admin = require("./Admin");
-const productRouter = require('./product.js');
+const productRouter = require("./product.js");
 const router = Router();
-const categoria = require("./category")
-const Cart = require("./Cart")
-const login = require("./login")
-const filter = require ("./filters")
+const categoria = require("./category");
+const Cart = require("./Cart");
+const login = require("./login");
+const filter = require("./filters");
+const email = require("./email");
 
-router.use('/products', productRouter);
+router.use("/products", productRouter);
 router.use("/users", user);
 router.use("/admin", admin);
 router.use("/categoria", categoria);
-router.use("/cart", Cart)
-router.use("/login", login)
-router.use("/filters", filter)
-
+router.use("/cart", Cart);
+router.use("/login", login);
+router.use("/filters", filter);
+router.use("/email", email);
 module.exports = router;
