b1136283e30cb0c14a97362893b029af8618ee76
vjose2807 <vjose2807@gmail.com>
2023-07-01 16:05:27 -0400

    conflicto de fusion en index reparado

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -6,12 +6,8 @@ const router = Router();
 const categoria = require("./category");
 const Cart = require("./Cart");
 const login = require("./login");
-<<<<<<< HEAD
-const filter = require("./filterAndSort");
-=======
 const filter = require("./filters");
 const email = require("./email");
->>>>>>> 813b0484310a0634d5eabaf48998dacc15cef7e4
 
 router.use("/products", productRouter);
 router.use("/users", user);
@@ -20,9 +16,5 @@ router.use("/categoria", categoria);
 router.use("/cart", Cart);
 router.use("/login", login);
 router.use("/filters", filter);
-<<<<<<< HEAD
-
-=======
 router.use("/email", email);
->>>>>>> 813b0484310a0634d5eabaf48998dacc15cef7e4
 module.exports = router;
