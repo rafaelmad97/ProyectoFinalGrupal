dd88b20c1f1b30a165b413f25742f165150938ac
juanmajustel <juanmajustel@gmail.com>
2023-07-13 16:32:16 -0300

    ultimos cambios

--- client/src/components/Filtros/FilterAndSort/FilterAndSort.jsx
+++ client/src/components/Filtros/FilterAndSort/FilterAndSort.jsx
@@ -1,15 +1,19 @@
-
-
 import { Box, Typography, MenuItem, Menu } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { filterByCategory, filterByDate, getAllCategorys } from "../../../redux/actions";
+import {
+  filterByCategory,
+  filterByDate,
+  getAllCategorys,
+} from "../../../redux/actions";
 
 const FilterAndSort = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { allCategorys, categoryFilter, dateFilter } = useSelector((state) => state);
+  const { allCategorys, categoryFilter, dateFilter } = useSelector(
+    (state) => state
+  );
   const [filterCategory, setFilterCategory] = useState("");
   const [anchorEl, setAnchorEl] = useState(null);
 
@@ -29,9 +33,9 @@ const FilterAndSort = () => {
   };
 
   const handleClick = () => {
-    dispatch(filterByDate())
-    navigate("/novedades")
-  }
+    dispatch(filterByDate());
+    navigate("/novedades");
+  };
 
   const handleClose = () => {
     setAnchorEl(null);
@@ -44,7 +48,7 @@ const FilterAndSort = () => {
       <Typography
         variant="h6"
         component="label"
-        sx={{ marginBottom: "8px", cursor: "pointer", marginLeft: "20px"  }}
+        sx={{ marginBottom: "8px", cursor: "pointer", marginLeft: "20px" }}
         onMouseEnter={handleMouseEnter}
       >
         Categorías
@@ -55,7 +59,6 @@ const FilterAndSort = () => {
         onClose={handleClose}
         elevation={1}
       >
-      
         {allCategorys?.map((cat) => (
           <MenuItem
             value={cat.name}
@@ -70,7 +73,7 @@ const FilterAndSort = () => {
         variant="h6"
         fontSize="18px"
         component="label"
-        sx={{ marginBottom: "8px", cursor: "pointer", marginLeft: "40px"  }}
+        sx={{ marginBottom: "8px", cursor: "pointer", marginLeft: "40px" }}
         onClick={handleClick}
       >
         Novedades
--- client/src/views/Landing/Landing.jsx
+++ client/src/views/Landing/Landing.jsx
@@ -1,13 +1,32 @@
-import { NavLink } from "react-router-dom";
-import styles from "./Landing.module.css";
+import React, { useState, useEffect } from "react";
+import "./Landing.css";
 
 const Landing = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simular carga durante 3 segundos
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+
+    // Limpiar el temporizador al desmontar el componente
+    return () => clearTimeout(timer);
+  }, []);
+
+  useEffect(() => {
+    if (!isLoading) {
+      // Redirigir a http://localhost:5173/ después de la carga
+      setTimeout(() => {
+        window.location.href = "http://localhost:5173/home";
+      }, 4000);
+    }
+  }, [isLoading]);
+
   return (
-    <div className={styles.page}>
-      <div className={styles.container}>
-        <NavLink to="/home" className={styles.link}>
-          Go To The Store
-        </NavLink>
+    <div>
+      <div className="container">
+        <div class="spinner"></div>
       </div>
     </div>
   );
